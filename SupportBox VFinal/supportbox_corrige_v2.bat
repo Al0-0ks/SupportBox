30a6d21ff4359bc9ed98423ad376eef76d53aeaa
Al0-0ks <aantonezak@gmail.com>
2026-05-28 14:05:03 +0200

    Delete SupportBox VFinal directory

--- SupportBox VFinal/supportbox_corrige_v2.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Force la console à utiliser l'encodage UTF-8 pour supporter les accents
-chcp 65001 > nul
-
-:: Lance le script PowerShell sans contourner la politique d'exécution globale.
-:: RemoteSigned est appliqué uniquement au processus courant.
-powershell.exe -NoProfile -ExecutionPolicy RemoteSigned -File "%~dp0supportbox_vF.ps1"
-
-pause

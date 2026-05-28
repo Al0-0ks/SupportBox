1458ffaa7c942ecd3260910990388ca1f45d36e6
Al0-0ks <aantonezak@gmail.com>
2026-05-28 14:04:22 +0200

    SupportVF.bat

--- SupportBox VFinal/supportbox_corrige_v2.bat
+++ SupportBox VFinal/supportbox_corrige_v2.bat
@@ -4,6 +4,6 @@ chcp 65001 > nul
 
 :: Lance le script PowerShell sans contourner la politique d'exécution globale.
 :: RemoteSigned est appliqué uniquement au processus courant.
-powershell.exe -NoProfile -ExecutionPolicy RemoteSigned -File "%~dp0supportbox_corrige_v2.ps1"
+powershell.exe -NoProfile -ExecutionPolicy RemoteSigned -File "%~dp0supportbox_vF.ps1"
 
 pause
